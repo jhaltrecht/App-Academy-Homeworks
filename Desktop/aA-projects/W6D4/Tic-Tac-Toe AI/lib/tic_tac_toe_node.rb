1e8b25a569d77ba51fa4baf12d37be8ea3153084
jared <jaredh9@gmail.com>
2020-01-24 11:01:14 -0800

    Children method complete and functioning

--- Desktop/aA-projects/W6D4/Tic-Tac-Toe AI/lib/tic_tac_toe_node.rb
+++ Desktop/aA-projects/W6D4/Tic-Tac-Toe AI/lib/tic_tac_toe_node.rb
@@ -1,39 +1,56 @@
 require_relative 'tic_tac_toe'
+require 'byebug'
 class TicTacToeNode
-  attr_reader :board,:next_mover_mark
+    attr_reader :board,:next_mover_mark, :prev_move_pos
 
-  def initialize(board, next_mover_mark, prev_move_pos = nil)
-    @board=board
-    @next_mover_mark=next_mover_mark
-    @prev_move_pos=prev_move_pos unless prev_move_pos.nil?
-  end
-
-  def losing_node?(evaluator)
-  end
-
-  def winning_node?(evaluator)
-  end
-
-  # This method generates an array of all moves that can be made after
-  # the current move.
-  def children
-    (0...@board.length).each do |row|
-        (0...@board.length).each do |col|
-            pos=[row,col]
-            if self[pos].empty
-            node=@board.dup
-            node.place_mark(pos,next_mover_mark)
-                if node.next_mover_mark== :x ? :o : :x)
-                  prev_move_pos=pos
-            end
-                end
-        end
+    def initialize(board, next_mover_mark, prev_move_pos = nil)
+      @board=board
+      @next_mover_mark=next_mover_mark
+      @prev_move_pos=prev_move_pos unless prev_move_pos.nil?
     end
-  end
 
-  def losing_node?(elevator)
+    def losing_node?(evaluator)
+      #  return true if evaluator.over? && evaluator.winner==next_mover_mark
+          return false if evaluator.winner.nil? || evaluator.winner ||elevator.tied?   
+      children.all?{|child| losing_node(child)} 
+      true
+    end
 
-  end
 
+    def winning_node?(evaluator)
+        return true if evaluator.winner 
+      children.any?{|child| losing_node(child)} 
+      false
+    end
+
+    # This method generates an array of all moves that can be made after
+    # the current move.
+    def children
+      next_mover_mark_temp=@next_mover_mark
+
+      nodes=[]
+      # debugger
+       (0...@board.rows.length).each do |row|
+          (0...@board.rows.length).each do |col|
+              if @board.rows[row][col].nil?
+                prev_move_pos_temp=[row,col]
+              nodeTemp=@board.dup
+              nodeTemp.rows[row][col]=next_mover_mark_temp
+              if next_mover_mark_temp==:x
+                next_mover_mark_temp=:o
+              else next_mover_mark_temp=:x
+              end
+              node=TicTacToeNode.new(nodeTemp,next_mover_mark_temp, prev_move_pos_temp)
+              nodes<<node
+              next_mover_mark_temp=@next_mover_mark
+              end
+          end
+       end
+     nodes
+    end
+
+  
 
 end
+
+

84b2c2f3dc481594dae6f44301ee66d173059426
jared <jaredh9@gmail.com>
2020-01-27 18:29:16 -0800

    finished implementation of manager class

--- Desktop/aA-projects/W6D5/ClassInheritance/employee.rb
+++ Desktop/aA-projects/W6D5/ClassInheritance/employee.rb
@@ -1,6 +1,6 @@
 class Employee
     attr_reader :name,:title,:salary,:boss
-    def initialize(name,title,salary,boss)
+    def initialize(name,title,salary,boss=nil)
         @name=name
         @title=title
         @salary=salary
@@ -13,17 +13,31 @@ class Employee
 end
 
 class Manager<Employee
+    attr_reader :employees
     def initialize(name,title,salary,boss)
         super
         @employees=[]
     end
 
     def bonus(multiplier)
-        bonus = @employees.sum * multiplier
+        bonus=0
+         @employees.each do |employee|
+            if isABoss?(employee)
+                employeeBonus=employee.bonus(multiplier)
+             bonus+= employee.salary * multiplier + employeeBonus
+            else  bonus+= employee.salary * multiplier 
+            end
+        end
+        bonus
     end
 
     def addEmployee(employee)
     @employees<<employee if employee.boss==self.name
+    end
+
+    def isABoss?(employee)
+        return true if employee.is_a?(Manager)
+        false
     end
 
 end
@@ -33,9 +47,10 @@ shawna=Employee.new("Shawna","TA",12000,"Darren")
 
 darren=Manager.new("Darren","TA Manager",78000,"Ned")
 ned=Manager.new("Ned","Founder",1000000,nil)
-darren.addEmployee(David)
-darren.addEmployee(Shawna)
-ned.bonus(5) # => 500_000
-darren.bonus(4) #D => 88_000
-david.bonus(3) # => 30_000
+darren.addEmployee(david)
+darren.addEmployee(shawna)
+ned.addEmployee(darren)
+p ned.bonus(5) # => 500_000
+p darren.bonus(4) #D => 88_000
+p david.bonus(3) # => 30_000
 

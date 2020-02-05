b3d8476265603f59f5446f0964c8589435d59545
jared <jaredh9@gmail.com>
2020-02-05 15:05:16 -0800

    made slight modifications to shuffle test

--- Desktop/aA-projects/W7D4/lib/deck.rb
+++ Desktop/aA-projects/W7D4/lib/deck.rb
@@ -17,6 +17,7 @@ class Deck
 
     def shuffle!
         @cards.shuffle!
+        @cards
     end
 
 end
--- Desktop/aA-projects/W7D4/spec/deck_spec.rb
+++ Desktop/aA-projects/W7D4/spec/deck_spec.rb
@@ -1,14 +1,11 @@
 require 'deck'
+require 'byebug'
 
 describe "Deck" do
     subject(:deck){Deck.new}
     describe "#initialize" do
         it "should have 52 cards" do
             expect(deck.cards.length).to eq(52)
-        end
-
-        it "should be able to shuffle the cards" do 
-            expect(deck.cards).to_not eq(deck.shuffle!)
         end
 
         it "should have 13 cards per suit" do
@@ -28,6 +25,11 @@ describe "Deck" do
             end
             expect(count.keys.length).to eq(4)
         end
+    end
+
+    it "should be able to shuffle the cards" do 
+        original_cards=deck.cards.dup
+        expect(original_cards).to_not eq(deck.shuffle!)
     end
     
 end

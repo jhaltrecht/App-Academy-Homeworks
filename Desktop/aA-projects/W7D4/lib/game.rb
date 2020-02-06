65845853e947057a7af066403d75e32d4dcb994e
jared <jaredh9@gmail.com>
2020-02-06 15:21:02 -0800

    implemented morechecks for game

--- Desktop/aA-projects/W7D4/lib/game.rb
+++ Desktop/aA-projects/W7D4/lib/game.rb
@@ -1,11 +1,23 @@
 require_relative "deck"
+require_relative "player"
 class Game
     attr_reader :deck,:players,:total,:pot
     def initialize
         @deck=Deck.new
-        debugger
         @players=[]
         @total=10,000
         @pot=0
+    end
+
+    def add_to_pot(num)
+        @pot+=num
+    end
+
+    def add_player(name)
+        @players<<Player.new(name)
+    end
+
+    def player_name(idx)
+        @players[idx].name
     end
 end
--- Desktop/aA-projects/W7D4/spec/game_spec.rb
+++ Desktop/aA-projects/W7D4/spec/game_spec.rb
@@ -16,6 +16,10 @@ subject(:game){Game.new}
         it 'should start with a full deck' do
             expect(game.deck.length).to eq(52)
         end
+
+        it 'should set up an empty pot' do
+            expect(game.pot).to eq(0)
+        end
     end
 
     describe "#turn" do
@@ -23,23 +27,31 @@ subject(:game){Game.new}
 
         end
     end
-    describe "#pot" do
-        it "should keep track of the amount in the pot" do
-            expect(game.pot).to be_an_instance_of(Game)
-        end
+    describe "#add_to_pot" do
+        it 'should add the amount to the pot' do
+            pot=game.pot
+            expect(game.add_to_pot(100)).to eq(pot+100)
+    end
     end
 
     describe "#add_player" do
         it "should add the player to the players array" do
-            game.add_player
-            expect(game.players.length).to eq(3)
+            game.add_player("name")
+            expect(game.players.length).to eq(1)
         end
+            let(:player){double("player")}
 
         it "should add the players name " do
-            let(:player){double("player")}
             expect(player).to receive(:name).with("Phil Helmuth")
             game.add_player("Phil Helmuth")
-            en
+        end
+    end
+
+    describe "#player_name" do 
+        it "should let you get the players name without having to call '.name' on the player class" do
+            game.add_player("name")
+            debugger
+            expect(game.player_name(0)).to eq(game.players[0].name)
         end
     end
 end

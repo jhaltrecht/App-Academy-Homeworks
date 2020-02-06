dc7190d6f31096ae2ebe2a9f34c2900572ba20c7
jared <jaredh9@gmail.com>
2020-02-06 13:22:03 -0800

    adding statements for game and player spec

--- Desktop/aA-projects/W7D4/spec/game_spec.rb
+++ Desktop/aA-projects/W7D4/spec/game_spec.rb
@@ -1,7 +1,16 @@
 require 'game'
 
 describe "Game" do 
-    
 
+
+    
+    it "should hold the deck" do
+        expect 
+    end
+    it "should track of whose turn it is"do 
+    end
+    it "should keep track of the amount in the pot" do
+
+    end
 
 end
--- Desktop/aA-projects/W7D4/spec/player_spec.rb
+++ Desktop/aA-projects/W7D4/spec/player_spec.rb
@@ -1,1 +1,33 @@
 require 'player'
+
+describe "Player" do 
+    let(:player){Player.new}
+    let(:hand){double("hand")}
+    let(:card){double("card")}
+
+    describe "#intialize" do 
+
+    end
+
+    describe "#make_move" do 
+    it "should assume the player chooses valid cards to discard" do 
+        expect{card.rank}.to be_between(0,14)
+    end
+
+    it "should tell the player to choose again if the cards are not valid" do
+        expect{}
+    end
+
+end
+
+    describe "#fold" do 
+    end
+
+    describe "#call" do 
+    end
+
+    describe "#raise" do 
+    end
+
+
+end

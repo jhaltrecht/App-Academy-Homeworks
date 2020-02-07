4475a948ab5040361153775a9a929a04ecc41409
jared <jaredh9@gmail.com>
2020-02-07 10:19:17 -0800

    not finished, moving on for now

--- Desktop/aA-projects/W7D4/lib/player.rb
+++ Desktop/aA-projects/W7D4/lib/player.rb
@@ -1,12 +1,13 @@
 class Player
     attr_reader :name,:stack
-    attr_accessor :hand
+    attr_accessor :hand,:active
     def initialize(name)
         @name=name
         @stack=500
+        @active=true
     end
 
-    def make_move
+    def make_move(amount)
         move='z'
         until move=='f'||move=='c'||move=='r'
             puts "Do you wish to fold, call, or raise. 'f' to fold, 'c' to call,'r' to raise"
@@ -15,8 +16,12 @@ class Player
             puts "invalid action" unless move=='c' && move=='r' && move=='f'
         end
         fold if move=='f' 
-        call if move=='c' 
-        self.raise if move=='r' 
+        call(amount) if move=='c' 
+         if move=='r' 
+            puts "how much would you like to raise"
+            raise_amount=gets.chomp.to_i
+            raise(raise_amount+amount)
+         end
     end
 
     def swap_move
@@ -24,28 +29,34 @@ class Player
         discard_count=0
         until pos=="s"
             puts " Enter a position of a card to discard: or 's' to skip "
+            pos=gets.chomp.to_i
             break if pos=='s'
             pos=pos.to_i
             if pos.between?(0,4) 
                 hand.discard(pos)
                 discard_count+=1
-            else puts "invalid position, try again:" 
+            else raise "invalid position, try again:" 
             end
         end
         return discard_count
     end
 
     def call(amount)
-        @stack-amount
+        raise 'not enough money' if @stack-amount<0
+        @stack-=amount
     end
 
     def raise(amount)
-        puts "how much would you like to raise?"
-        raise_amount=gets.input.map(&:to_i)
-        return 
+        raise 'not enough money to raise' if @stack-amount<0
+        @stack-=amount
+        return amount
     end
 
     def fold
+        @active=false
+    end
 
+    def receive(winnings)
+        @stack+=winnings
     end
 end

0039ffbeff0e05976f655e38c4f35ff4744c6516
jared <jaredh9@gmail.com>
2020-02-24 11:48:54 -0800

    tested first 2 methods of course

--- Desktop/aA-projects/W10D1/Association_Exercise/app/models/course.rb
+++ Desktop/aA-projects/W10D1/Association_Exercise/app/models/course.rb
@@ -1,2 +1,9 @@
 class Course < ApplicationRecord
+    has_many(
+        :enrollments,
+        class_name: "Enrollment",
+        foreign_key: :student_id,
+        primary_key: :id
+)
+    has_many :enrolled_students, through: :enrollments, source: :user
 end

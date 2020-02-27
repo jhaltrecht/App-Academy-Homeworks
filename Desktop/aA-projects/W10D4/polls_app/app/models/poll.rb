2d7faf16f4f490ab0e4fd622c6524133600fa637
jared <jaredh9@gmail.com>
2020-02-27 14:22:32 -0800

    first 2 migrations complete

---
+++ Desktop/aA-projects/W10D4/polls_app/app/models/poll.rb
@@ -0,0 +1,11 @@
+class Poll < ApplicationRecord
+    validates :author_id,:title, presence: true
+
+belongs_to :author,
+class_name: 'User',
+foreign_key: :author_id,
+primary_key: :id
+
+
+
+end
---
+++ Desktop/aA-projects/W10D4/polls_app/app/models/user.rb
@@ -0,0 +1,9 @@
+class User <ApplicationRecord
+    validates :username, presence: true, uniqueness: true
+
+
+
+
+
+
+end

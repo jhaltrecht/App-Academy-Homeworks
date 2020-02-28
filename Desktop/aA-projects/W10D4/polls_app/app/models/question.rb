f6afffe11e56c759e91aff36120cf31aa8f5973c
jared <jaredh9@gmail.com>
2020-02-28 12:20:34 -0800

    SQL results completed

--- Desktop/aA-projects/W10D4/polls_app/app/models/question.rb
+++ Desktop/aA-projects/W10D4/polls_app/app/models/question.rb
@@ -15,8 +15,41 @@ class Question < ApplicationRecord
     has_many :responses, through: :answer_choices, source: :responses
 # first with n+1 query
     def results
+            choices_counts={}
+
+        ans_choices=self.answer_choices
+
+        ans_choices.each do |choice|
+            choices_counts[choice.text]=choice.responses.count
+        end
+    end
+
+    def includes_results
+         choices_counts={}
+         ans_choices=self.answer_choices.includes(:responses)
+         ans_choices.each do |choice|
+            choices_counts[choice.text]=choice.responses.length
+        end
 
     end
+
+    def results_SQL
+        Response.find_by_sql
+        ans_choice=AnswerChoice.find_by_sql([<<-SQL, id])
+        SELECT
+            answer_choices.*,COUNT(responses.id) as Response_Count
+        FROM 
+            answer_choices
+        LEFT JOIN responses
+            on answer_choices.id=responses.answer_choice_id
+        WHERE 
+            answer_choices.question_id=?
+        GROUP BY
+            answer_choice.id
+        SQL
+    end
+
+    # sql query
 
 
 

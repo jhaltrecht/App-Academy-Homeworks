3ce04d5fdaf4671439b87108eac3d4f85f02b7d1
jared <jaredh9@gmail.com>
2020-03-02 12:49:11 -0800

    phase 0 complete

---
+++ Desktop/aA-projects/W10D5/OwnActiveRecord/lib/00_attr_accessor_object.rb
@@ -0,0 +1,8 @@
+class AttrAccessorObject
+  def self.my_attr_accessor(*names)
+    names.each do |name|
+      define_method("#{name}") { self.instance_variable_get("@#{name}")}
+      define_method("#{name}=") { |other_val| self.instance_variable_set("@#{name}",other_val)}
+    end
+  end
+end

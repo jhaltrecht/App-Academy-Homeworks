d258476bcbe5d022402554b3f8dc7fa7ac210a6c
jared <jaredh9@gmail.com>
2020-03-06 15:06:49 -0800

    removed name column from users

--- Desktop/aA-projects/W11D2/my_project_name/db/schema.rb
+++ Desktop/aA-projects/W11D2/my_project_name/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_06_223653) do
+ActiveRecord::Schema.define(version: 2020_03_06_230557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 2020_03_06_223653) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username", null: false

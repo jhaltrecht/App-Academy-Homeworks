c473c3752fc1161bb81a45284d5b2ffc5ab2efbf
jared <jaredh9@gmail.com>
2020-04-10 11:45:01 -0700

    fizzBuzz complete

--- W13D1/intro_JS_problems.js
+++ W13D1/intro_JS_problems.js
@@ -8,5 +8,17 @@ function isSubstring(searchString, subString){
     return searchString.includes(subString);
 }
 
-console.log(isSubstring("time to program", "time"));
-console.log(isSubstring("Jump for joy", "joys"));
+// console.log(isSubstring("time to program", "time"));
+// console.log(isSubstring("Jump for joy", "joys"));
+
+function fizzBuzz(array) {
+    newArray=[];
+    array.forEach((element)=> {
+        if (element%3===0 || element%5===0)
+        newArray.push(element);
+    })
+    return newArray
+}
+
+array=[3,2,15,5,7]
+console.log(fizzBuzz(array))

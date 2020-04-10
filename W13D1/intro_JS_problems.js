50a5ebe3d4b5c1a2b08f37a321267de27dae6850
jared <jaredh9@gmail.com>
2020-04-10 13:05:35 -0700

    phase 1 complete

--- W13D1/intro_JS_problems.js
+++ W13D1/intro_JS_problems.js
@@ -26,24 +26,40 @@ function fizzBuzz(array) {
 function isPrime(num) {
     for(i=2; i<num; i++) {
          if (num%i===0) {
-             return false
+             return false;
          }
     }
-    return true
+    return true;
 }
 
-console.log(isPrime(2))
-console.log(isPrime(10))
-console.log(isPrime(15485863))
-console.log(isPrime(3548563))
+// console.log(isPrime(2))
+// console.log(isPrime(10))
+// console.log(isPrime(15485863))
+// console.log(isPrime(3548563))
 
 function sumOfNPrimes(num) {
-    
+    let sum=0;
+    firstNPrimes(num).forEach(element => {
+       sum+=element ;
+    });
+    return sum;
 }
 
 function firstNPrimes(num) {
-    
+    var count=0;
+    var i=2;
+    primeArr=[];
+    while (count<num) {
+        if (isPrime(i)) {
+        primeArr.push(i)
+        count+=1
+        }
+        ++i;
+    }
+        return primeArr;
 }
+
+// console.log(firstNPrimes(4))
 
 console.log(sumOfNPrimes(0))
 console.log(sumOfNPrimes(1))

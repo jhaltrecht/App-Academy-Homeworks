5e2741b4d0bf768366f7bbc4134852169374a52e
jared <jaredh9@gmail.com>
2020-04-17 11:53:51 -0700

    project complete for practice.js

--- W13D3/canvas_practice/practice.js
+++ W13D3/canvas_practice/practice.js
@@ -3,7 +3,15 @@ document.addEventListener("DOMContentLoaded", function(){
     canvasEl.width=500;
     canvasEl.height=500;
     const ctx=canvasEl.getContext("2d");
-    ctx.fillStyle='red'
-    ctx.fillRect(0,0,500,500)
-    ctx.beginPath()
+    ctx.fillStyle='red';
+    ctx.fillRect(0,0,500,500);
+
+    ctx.beginPath();
+    ctx.arc(100, 100, 40, 0, 2*Math.PI, true);
+    ctx.strokeStyle='purple';
+    ctx.lineWidth=5;
+    ctx.stroke();
+    ctx.fillStyle='orange';
+    ctx.fill();
+
 });

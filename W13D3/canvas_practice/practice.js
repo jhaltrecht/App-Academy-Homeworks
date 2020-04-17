d935c750f7f67610b42c6c339813027344079659
jared <jaredh9@gmail.com>
2020-04-17 11:32:25 -0700

    updating practice.js

--- W13D3/canvas_practice/practice.js
+++ W13D3/canvas_practice/practice.js
@@ -1,3 +1,9 @@
 document.addEventListener("DOMContentLoaded", function(){
-
+    const canvasEl = document.getElementById("mycanvas");
+    canvasEl.width=500;
+    canvasEl.height=500;
+    const ctx=canvasEl.getContext("2d");
+    ctx.fillStyle='red'
+    ctx.fillRect(0,0,500,500)
+    ctx.beginPath()
 });

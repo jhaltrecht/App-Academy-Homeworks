62b4f44dbdfefc4b5ceb64925566711ec95f3a71
jared <jaredh9@gmail.com>
2020-04-28 16:23:19 -0700

    homework complete

--- W15D2/Vanilla_Dom_Manipulation/script.js
+++ W15D2/Vanilla_Dom_Manipulation/script.js
@@ -17,15 +17,34 @@ document.addEventListener("DOMContentLoaded", () => {
 
 
   // adding SF places as list items
+  const addLi=(e) =>{
+    e.preventDefault();
+    const nameInput = document.querySelector(".favorite-input")
+    const nameInputVal = nameInput.value;
+    nameInput.value = "";
+    const li = document.createElement("li");
+    li.textContent = nameInputVal;
 
+    const ul = document.getElementById("sf-places");
+    ul.appendChild(li);
+  };
   // --- your code here!
-
+const listSubmitButton=document.querySelector(".favorite-submit");
+listSubmitButton.addEventListener("click", addLi);
 
 
   // adding new photos
-
+  const togglePhotoForm = (e) => {
+    const photoFormDiv = document.querySelector(".photo-form-container");
+    if (photoFormDiv.className === "photo-form-container") {
+      photoFormDiv.className = "photo-form-container hidden";
+    } else {
+      photoFormDiv.className = "photo-form-container";
+    }
+  };
   // --- your code here!
-
+  const photoShowButton = document.querySelector(".photo-show-button")
+  photoShowButton.addEventListener("click",togglePhotoForm)
 
 
 });
